e826a03d1c376ad07d11f778dc08c9f3fc606002
TRGanesh <117368449+TRGanesh@users.noreply.github.com>
2023-06-19 00:35:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# IMPORTING REQUIRED LIBRARIES
+import streamlit as st
+#import preprocessor
+#import helper
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.express as px
+
+def main():
+    st.title('hi')
+    
+if __name__ == '__main__':
+    main()    
